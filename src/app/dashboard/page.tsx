21be682a08d2942dc1ddbfb1511c81dad7e02724
gaurav <gauraviscoding10@gmail.com>
2025-03-25 11:04:53 +0530

    added dashboard component, fixed login and logout logic, added homepage component, in dashboard added multiple card components which describes history, recent activity, latest hot topics and quiz me tracker card

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { getAuthSession } from '@/lib/nextauth'
+import { redirect } from 'next/navigation';
+import DetailsDialog from '@/components/details-dialog';
+import QuizMeCard from '@/components/dashboard/quiz-me-card';
+import HistoryCard from '@/components/dashboard/history-card';
+// import HotTopicsCard from '@/components/dashboard/hot-topics-card';
+import RecentActivityCard from '@/components/dashboard/recent-activity-card';
+
+
+export const metadata = {
+    title: "Dashboard | Quizmify"
+}
+
+const Dashboard = async () => {
+    const session = await getAuthSession();
+
+    if(!session?.user){
+        return redirect("/")
+    }
+  return (
+    <main className="p-8 mx-auto max-w-7xl">
+      <div className="flex items-center">
+        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+        <DetailsDialog />
+      </div>
+
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizMeCard />
+        <HistoryCard />
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+        {/* <HotTopicsCard /> */}
+        <RecentActivityCard />
+      </div>
+    </main>
+  )
+}
+
+export default Dashboard
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,15 +1,16 @@
 "use client"
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes';
 import React from 'react'
 
-type Props = {
-    children: React.ReactNode;
-}
-
-const Providers = ({children}: Props) => {
+const Providers = ({ children }: ThemeProviderProps) => {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemesProvider 
+      attribute="class" 
+      defaultTheme="system" 
+      enableSystem
+    >
         <SessionProvider>{children}</SessionProvider>
     </NextThemesProvider>
   )
